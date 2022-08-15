b9787545838bef9c7846b4a94c52a586ed559918
Александр <sh889@ya.ru>
2022-08-15 20:28:30 +0500

    Превью для фото

--- technoshop-cms/js/index.js
+++ technoshop-cms/js/index.js
@@ -1,5 +1,6 @@
 import { modalBtn, modal } from './elems.js';
 import { modalController } from './modalController.js';
+import { previewController } from './previewController.js';
 
 modalController({
   modal,
@@ -7,3 +8,5 @@ modalController({
   classOpen: 'd-block',
   classClose: 'btn-close',
 });
+
+previewController();
---
+++ technoshop-cms/js/previewController.js
@@ -0,0 +1,17 @@
+import { form, preview } from "./elems.js";
+import { toBase64 } from "./utils.js";
+
+const showPreview = (src) => {
+  preview.style.display = 'block';
+  preview.src = src;
+};
+
+export const previewController = () => {
+  const imageFile = form.image;
+  image.addEventListener('change', async () => {
+    if (imageFile.files.length) {
+      const src = await toBase64(imageFile.files[0]);
+      showPreview(src);
+    }
+  });
+};
