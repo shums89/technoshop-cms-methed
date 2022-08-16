98560a255fbd409abb97c03d858cb2a5a46a6a94
Александр <sh889@ya.ru>
2022-08-16 21:53:17 +0500

    День 2. Получение данных через API

--- technoshop-cms/js/index.js
+++ technoshop-cms/js/index.js
@@ -1,12 +1,20 @@
 import { modalBtn, modal } from './elems.js';
+import { formController } from './formController.js';
 import { modalController } from './modalController.js';
 import { previewController } from './previewController.js';
+import { tableController } from './tableController.js';
 
-modalController({
-  modal,
-  btn: modalBtn,
-  classOpen: 'd-block',
-  classClose: 'btn-close',
-});
+const init = () => {
+  modalController({
+    modal,
+    btn: modalBtn,
+    classOpen: 'd-block',
+    classClose: 'btn-close',
+  });
 
-previewController();
+  previewController();
+  tableController();
+  formController();
+};
+
+init();
---
+++ technoshop-cms/js/tableController.js
@@ -0,0 +1,8 @@
+import { getGoods } from "./serviceAPI.js";
+import { tableRender } from "./tableView.js";
+
+export const tableController = async () => {
+  const goods = await getGoods();
+
+  tableRender(goods);
+};
